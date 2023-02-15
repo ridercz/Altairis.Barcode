3426fcc02c3f767ee64412c84badc18189471b9c
Michal Altair Valášek <github@rider.cz>
2023-02-15 20:23:23 +0100

    Test all supported variants in demo app.

--- Altairis.Barcode.TestApp/Program.cs
+++ Altairis.Barcode.TestApp/Program.cs
@@ -4,7 +4,8 @@ using Altairis.Barcode;
 CreateBarcode(new Code25IndustrialGenerator("1234567"), "barcode-25-industrial.png");
 CreateBarcode(new Code25InterleavedGenerator("1234567"), "barcode-25-interleaved.png");
 CreateBarcode(new Code25IataGenerator("1234567"), "barcode-25-iata.png");
-CreateBarcode(new Code39StandardGenerator("ALTAIR"), "barcode-code39.png");
+CreateBarcode(new Code39StandardGenerator("ALTAIR"), "barcode-39-standard.png");
+CreateBarcode(new Code39FullAsciiGenerator("Altair"), "barcode-39-fullascii.png");
 CreateBarcode(new Ean8Generator("1234567"), "barcode-ean-8.png");
 CreateBarcode(new Ean13Generator("123456789012"), "barcode-ean-13.png");
 
